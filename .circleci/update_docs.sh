ccc1c71262f4b327a021a49d5e80ca4ea26b36d9
Paul Czarkowski <username.taken@gmail.com>
2019-07-02 15:49:43 -0500

    fix update_docs script
    Signed-off-by: Paul Czarkowski <username.taken@gmail.com>

--- .circleci/update_docs.sh
+++ .circleci/update_docs.sh
@@ -3,6 +3,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -s nullglob
 
 : "${GIT_REPOSITORY_URL:?Environment variable GIT_REPO_URL must be set}"
 : "${GIT_USERNAME:?Environment variable GIT_USERNAME must be set}"
@@ -13,9 +14,10 @@ git config user.name "$GIT_USERNAME"
 
 mkdir -p .deploy/readme
 cp --force README.md .deploy/readme/
-for file in `ls charts/*/*.md`; do
-    mkdir -p .deploy/readme/`dirname $file`
-    cp --force $file .deploy/readme/`dirname $file`
+
+for file in charts/*/*.md; do
+    mkdir -p ".deploy/readme/$(dirname "$file")"
+    cp --force "$file" ".deploy/readme/$(dirname "$file")"
 done
 
 git checkout gh-pages
